bfa1c1b2486446d99761f1ceb4857cf398953bcd
Serg <s@s.com>
2026-01-25 17:26:57 +0300

    Add validators in TelegramUser

--- tg_users/models.py
+++ tg_users/models.py
@@ -1,11 +1,39 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator, MinLengthValidator
+
+import re
+
+MAX_COUNT_CHARS = 50
 
 
 class TelegramUser(models.Model):
-    chat_id = models.CharField(max_length=50)
-    first_name = models.CharField(max_length=50)
-    second_name = models.CharField(max_length=50)
-    father_name = models.CharField(max_length=50)
-    
+    chat_id = models.CharField(
+        max_length=MAX_COUNT_CHARS,
+        validators=[
+            RegexValidator(
+                r"^\d+$",
+                message="Only digist",
+                code="invalid_digists",
+            ),
+            MinLengthValidator(2),
+        ]
+    )
+    first_name = models.CharField(
+        max_length=MAX_COUNT_CHARS,
+        validators=[MinLengthValidator(3)],
+    )
+    second_name = models.CharField(
+        max_length=MAX_COUNT_CHARS,
+        validators=[MinLengthValidator(3)]
+    )
+    father_name = models.CharField(
+        max_length=MAX_COUNT_CHARS,
+        validators=[MinLengthValidator(3)],
+    )
+
     def __str__(self):
         return f"{self.first_name} {self.second_name} {self.father_name}"
+
+    def clean(self):
+        ...
